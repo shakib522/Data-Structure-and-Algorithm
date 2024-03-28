0fba6470ab35bb456580c632c761eb3fa8f51621
shakib522 <shakibhasann522@gmail.com>
2024-03-29 01:45:07 +0600

    BST delete implemented

--- src/tree/BinarySearchTree.kt
+++ src/tree/BinarySearchTree.kt
@@ -65,11 +65,46 @@ class BstNode(val data:Int, var parent:BstNode?=null, var left:BstNode?=null, va
         }
         return root
     }
+
+    private fun bstMinimum(root:BstNode?):BstNode?{
+        var currentNode=root
+        while(currentNode?.left!=null){
+            currentNode=currentNode.left
+        }
+        return currentNode
+    }
+    private fun bstTransplant(root:BstNode?, currentNode:BstNode?, newNode:BstNode?):BstNode?{
+        var root2=root
+        when(currentNode){
+            root2-> root2=newNode
+            currentNode?.parent?.left -> addLeftChild(currentNode?.parent,newNode)
+            else -> addRightChild(currentNode?.parent,newNode)
+        }
+        return root2
+    }
+    fun bstDelete(root:BstNode?,node:BstNode?):BstNode?{
+        var root2=root
+        val smallestNode:BstNode?
+        if (node?.left==null){
+            root2=bstTransplant(root2,node,node?.right)
+        }else if(node.right==null){
+            root2=bstTransplant(root2,node,node.left)
+        }else{
+            smallestNode=bstMinimum(node.right)
+            if(smallestNode?.parent!=node){
+                root2= bstTransplant(root2,smallestNode,smallestNode?.right)
+                addRightChild(smallestNode,node.right)
+            }
+            root2=bstTransplant(root2,node,smallestNode)
+            addLeftChild(smallestNode,node.left)
+        }
+        return root2
+    }
 }
 
-fun createBST():BstNode{
-    var root:BstNode
-    var node:BstNode
+fun createBST():BstNode?{
+    var root:BstNode?
+    var node:BstNode?
     val ara= arrayOf(5,17,3,7,12,19,1,4)
     root= BstNode(10)
     for (item in ara){
@@ -95,25 +130,53 @@ fun bstSearch(root:BstNode?,item:Int):BstNode?{
 }
 
 fun main() {
-    val root= createBST()
-    root.preOrder(root)
+    var root= createBST()
+    root?.preOrder(root)
     println()
-    root.inOrder(root)
+    root?.postOrder(root)
     println()
-    root.postOrder(root)
+    println("In order: ")
+    root?.inOrder(root)
     println()
     var node= bstSearch(root,7)
     if (node!=null){
         println("data found in binary search tree: ${node.data}")
+        println("Will delete ${node.data}")
+        root=root?.bstDelete(root,node)
     }else{
         println("Data not found")
     }
+
+    println("BST: ")
+    root?.inOrder(root)
+    println()
     node= bstSearch(root,8)
     if (node!=null){
         println("data found in binary search tree: ${node.data}")
+        println("Will delete ${node.data}")
+        root=root?.bstDelete(root,node)
     }else{
         println("Data not found")
     }
+    node= bstSearch(root,5)
+    if (node!=null){
+        println("data found")
+        println("Will be delete ${node.data}")
+        root=root?.bstDelete(root,node)
+    }
+
+    println("BST: ")
+    root?.inOrder(root)
+
+    node= bstSearch(root,10)
+    if (node!=null){
+        println("data found")
+        println("Will be delete ${node.data}")
+        root=root?.bstDelete(root,node)
+    }
+
+    println("BST: ")
+    root?.inOrder(root)
 }
 
 
