5dbb407433aef68bc0a283e9d88066e54d84803c
shakib522 <shakibhasann522@gmail.com>
2024-05-01 02:15:53 +0600

    implement BFS using adjacency matrix

--- src/graph/BFS.kt
+++ src/graph/BFS.kt
@@ -25,6 +25,27 @@ class BFS{
         }
         return bfs
     }
+
+    fun bfsUsingAdjacencyMatrix(adj:Array<IntArray>,startNode:Int){
+        //Time Complexity: O(N*N)
+        //Auxiliary Space: O(N)
+        val visited=Array(adj.size){false}
+        val q=ArrayDeque<Int>()
+        val ansBfs= mutableListOf<Int>()
+        q.addFirst(startNode)
+        visited[startNode]=true
+        while (q.isNotEmpty()){
+            val node=q.removeFirst()
+            ansBfs.add(node)
+            for(i in 0..< adj[node].size){
+                if(adj[node][i]==1 && !visited[i]){
+                    q.addLast(i)
+                    visited[i]=true
+                }
+            }
+        }
+        println(ansBfs)
+    }
 }
 //auxiliary space complexity - O(3n) == O(n)
 //time complexity - O(n) + O(2E)  //the queue is run for all element.So it's complexity is O(N).
@@ -44,4 +65,14 @@ fun main() {
     adj.add(arrayListOf(4,7))
     println("--BFS--")
     println(bfs.bfsOfGraph(9,adj))
+
+//  0 based graph
+    val isConnected= Array(5){IntArray(5)}
+    isConnected[0]= intArrayOf(0,1,0,0,1)
+    isConnected[1]= intArrayOf(1,0,1,1,0)
+    isConnected[2]= intArrayOf(0,1,0,0,0)
+    isConnected[3]= intArrayOf(0,1,0,0,0)
+    isConnected[4]= intArrayOf(1,1,0,0,0)
+    println("--BFS using adjacency matrix ---")
+    bfs.bfsUsingAdjacencyMatrix(isConnected,0)
 }
