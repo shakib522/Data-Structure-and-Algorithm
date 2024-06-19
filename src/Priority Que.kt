836ff372e61e5fdfca9cf167d0591c301d363121
shakib522 <shakibhasann522@gmail.com>
2024-06-19 12:10:13 +0600

    Diajkstra using priority queue implemented

--- src/Priority Que.kt
+++ src/Priority Que.kt
@@ -1,12 +1,32 @@
 import heap.maxHeapify
 import heap.parent
+import java.util.*
+import kotlin.Comparator
 
 
 fun main() {
-
+    val pq = PriorityQueue<Int>()
+    pq.add(3)
+    pq.add(12)
+    pq.add(8)
+    pq.add(1)
+    pq.add(100)
+    pq.add(11)
+    pq.add(2)
+    println(pq)
+    println(pq.poll())
+    println(pq.poll())
+    //the all integer value are inserted using min heap.That's why we get value according to min heap when we print the priority queue
+    val comparator = Comparator<Pair<Int,Int>>{ p1,p2 ->
+        p1.second-p2.second
+    }
+    val pq2=PriorityQueue(comparator)
+    pq2.add(Pair(3,2))
+    pq2.add(Pair(4,1))
+    println(pq2.peek())
 }
 
-class PriorityQue(var size:Int) {
+class PriorityQue(var size: Int) {
     fun extractMax(heap: IntArray): Int {
         //time complexity of this function: O(log n)
         val maxItem = heap[1]
--- src/graph/Dijkstra Algorithm.kt
+++ src/graph/Dijkstra Algorithm.kt
@@ -1,7 +1,63 @@
 package graph
 
+import java.util.*
+
+
+fun main() {
+
+    // Example usage
+    val V = 5 // Number of vertices
+    val S = 0 // Source node
+    val adj = ArrayList<ArrayList<ArrayList<Int>>>()
+
+
+    // Initialize adjacency list
+    for (i in 0 until V) {
+        adj.add(ArrayList())
+    }
+
+
+    // Add edges (example graph)
+    adj[0].add(ArrayList(mutableListOf(1, 2)))
+    adj[0].add(ArrayList(mutableListOf(3, 1)))
+    adj[1].add(ArrayList(mutableListOf(2, 3)))
+    adj[1].add(ArrayList(mutableListOf(0, 2)))
+    adj[2].add(ArrayList(mutableListOf(3, 1)))
+    adj[2].add(ArrayList(mutableListOf(1, 3)))
+    adj[3].add(ArrayList(mutableListOf(4, 6)))
+    adj[3].add(ArrayList(mutableListOf(0, 1)))
+    adj[3].add(ArrayList(mutableListOf(2, 1)))
+
+    val distances = dijkstra(V, adj, S)
+
+    println("Shortest distances from source $S to all nodes:")
+    for (i in distances.indices) {
+        println("Node " + i + ": " + distances[i])
+    }
+}
 
 fun dijkstra(V: Int, adj: ArrayList<ArrayList<ArrayList<Int>>>, S: Int): IntArray {
     // Write your code here
-    return intArrayOf()
+    // weighted undirected (cyclic or acyclic) connected graph
+    val comparator = Comparator<Pair<Int, Int>> { p1, p2 ->
+        p1.first - p2.first
+    }
+    val pq = PriorityQueue(comparator)
+    val dis: Array<Int> = Array(V) { 1000000000 } //10^8 or 1e9
+    pq.add(Pair(0, S))
+    dis[S] = 0
+    while (pq.isNotEmpty()) {
+        val distance = pq.first().first
+        val node = pq.first().second
+        pq.poll()
+        for (it in adj[node]) {
+            val edgeWeight = it[1]
+            val adjNode = it[0]
+            if (distance+edgeWeight  < dis[adjNode]){
+                dis[adjNode] = distance+edgeWeight
+                pq.add(Pair(dis[adjNode],adjNode))
+            }
+        }
+    }
+    return dis.toIntArray()
 }
