4b53659b6ab21ab81b4c43b952515b45a52f2c84
shakib522 <shakibhasann522@gmail.com>
2024-07-20 13:00:00 +0600

    implement SelectionSort algorithm

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -2,28 +2,47 @@ package sorting;
 
 import java.util.Arrays;
 
-public class SelectionSort{
+public class  SelectionSort{
     public static void main(String[] args) {
-        int[] arr={6,4,2,1,0};
-        selection(arr,arr.length,0,0);
+        int[] arr={6,4,2,1,0,3,3};
+        int[] arr2={6,4,2,1,0,8,8,3};
+        selectionSortUsingRecursion(arr,arr.length,0,0);
+        selectionSort(arr2,arr2.length);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void selection(int[] arr,int r,int c,int max){
+    static void selectionSortUsingRecursion(int[] arr,int r,int c,int max){
         if (r==0){
             return;
         }
         if (c<r){
             if (arr[c]>arr[max]) {
-                selection(arr, r, c + 1, c);
+                selectionSortUsingRecursion(arr, r, c + 1, c);
             }else{
-                selection(arr,r,c+1,max);
+                selectionSortUsingRecursion(arr,r,c+1,max);
             }
         }else{
             int temp=arr[r-1];
             arr[r-1]=arr[max];
             arr[max]=temp;
-            selection(arr,r-1,0,0);
+            selectionSortUsingRecursion(arr,r-1,0,0);
+        }
+    }
+
+    static void selectionSort(int[] arr,int n){
+        int temp,index_min;
+        for (int i=0;i<n-1;i++){
+            index_min=i;
+            for (int j=i+1;j<n;j++){
+                if (arr[j]<arr[index_min]){
+                    index_min=j;
+                }
+            }
+            if (index_min!=i){
+                temp=arr[i];
+                arr[i]=arr[index_min];
+                arr[index_min]=temp;
+            }
         }
     }
 }
